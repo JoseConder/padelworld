fe161d09fa7473bbea342a89cffa71227ffce144
joseconde <a299506@uach.mx>
2024-05-06 19:16:19 -0600

    cambios por seguridad

--- frontpw/credenciales.js
+++ frontpw/credenciales.js
@@ -3,15 +3,15 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC75aSogBYV9ftfOUI8j7U3upNoqP--cdY",
-    authDomain: "padelworld-62d27.firebaseapp.com",
-    projectId: "padelworld-62d27",
-    storageBucket: "padelworld-62d27.appspot.com",
-    messagingSenderId: "842503758444",
-    appId: "1:842503758444:web:b1867022356693399c6e49",
-    measurementId: "G-WMB68YCM44"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 };
-
+  
 const appFirebase = initializeApp(firebaseConfig);
 const appFirebaseAuth = initializeAuth(appFirebase, {
     persistence: getReactNativePersistence(ReactNativeAsyncStorage)
